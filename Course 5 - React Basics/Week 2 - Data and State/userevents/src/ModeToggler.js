06d3f014217183f0ed8fd34714601e002d1aa4f7
giovannitoledo1 <giovanni.toledo507@myci.csuci.edu>
2023-03-18 21:29:58 -0700

    Week 2 user event done 2

--- Course 5 - React Basics/Week 2 - Data and State/userevents/src/ModeToggler.js
+++ Course 5 - React Basics/Week 2 - Data and State/userevents/src/ModeToggler.js
@@ -3,9 +3,21 @@ function ModeToggler(){
    const darkMode = <h1>Dark Mode is on</h1>
    const lightMode = <h1>Light Mode is On</h1>
 
+   function handleClick(){
+        darkModeOn = !darkModeOn;
+        if(darkModeOn == true){
+            console.log("Dark mode is on")
+        } else {
+            console.log("Light mode is")
+        }
+   }
+
     return(
         <div>
             {darkModeOn ? darkMode : lightMode}
+            <button onClick={handleClick}>
+                Click me!
+            </button>
         </div>
     )
 }
