c90cc5540f29018fd78bcd51d62a1eb4a60ecfb9
giovannitoledo1 <giovanni.toledo507@myci.csuci.edu>
2023-03-18 21:18:11 -0700

    Week 2 dynamic changes

---
+++ Course 5 - React Basics/Week 2 - Data and State/commoneventhandling/components/Btn.js
@@ -0,0 +1,10 @@
+function Btn(){
+    const clickHandler = () => console.log('Clicked!');
+    return(
+        <button onClick={clickHandler}>
+            Click me!
+        </button>
+    );
+};
+
+export default Btn;
